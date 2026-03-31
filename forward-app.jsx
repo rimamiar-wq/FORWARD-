a040fbac3e45bb0a298b3012a84be3c88c018958
rimamiar-wq <rimamiar@gmail.com>
2026-03-31 16:27:44 -0400

    Add ForwardApp component

---
+++ forward-app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ForwardApp = () => {
+    return (
+        <div>
+            <h1>Welcome to the Forward App!</h1>
+        </div>
+    );
+};
+
+export default ForwardApp;
